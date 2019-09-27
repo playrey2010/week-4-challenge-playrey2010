2abd592b087847b6595695a4d8dafaf8b7c474e5
playrey2010 <playrey2010@gmail.com>
2019-09-27 10:50:36 -0400

    finished

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -21,28 +21,53 @@ public class HomeController {
         return "profile";
     }
 
-    @RequestMapping("/github")
-    public String github() {
-        return "github";
+    @RequestMapping("/github1")
+    public String github1() {
+        return "github1";
     }
 
-    @RequestMapping("/prodesign")
-    public String proDesign() {
-        return "PD&A";
+    @RequestMapping("/github2")
+    public String github2() {
+        return "github2";
     }
 
-    @RequestMapping("/corejava")
-    public String java() {
-        return "corejava";
+    @RequestMapping("/prodesign1")
+    public String proDesign1() {
+        return "prodesign1";
     }
 
-    @RequestMapping("/oojava")
-    public String oojava() {
-        return "oojava";
+    @RequestMapping("/prodesign2")
+    public String proDesign2() {
+        return "prodesign2";
     }
 
-    @RequestMapping("/webdesign")
-    public String webdesign() {
-        return "webdesign";
+    @RequestMapping("/corejava1")
+    public String java1() {
+        return "corejava1";
+    }
+
+    @RequestMapping("/corejava2")
+    public String java2() {
+        return "corejava2";
+    }
+
+    @RequestMapping("/oojava1")
+    public String oojava1() {
+        return "oojava1";
+    }
+
+    @RequestMapping("/oojava2")
+    public String oojava2() {
+        return "oojava2";
+    }
+
+    @RequestMapping("/webdesign1")
+    public String webdesign1() {
+        return "webdesign1";
+    }
+
+    @RequestMapping("/webdesign2")
+    public String webdesign2() {
+        return "webdesign2";
     }
 }
